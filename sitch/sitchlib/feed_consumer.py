94eaba895a5f9dd52ca54249f70549d3bd7577cd
Ash Wilson <ash.d.wilson@gmail.com>
2017-06-11 16:24:34 -0700

    chg: usr: Switch to Mozilla Location Services for BTS reference feed.

--- sitch/sitchlib/feed_consumer.py
+++ sitch/sitchlib/feed_consumer.py
@@ -1,5 +1,6 @@
 import gzip
 import os
+import re
 import requests
 import shutil
 from zipfile import ZipFile
@@ -14,13 +15,15 @@ class FeedConsumer(object):
     """
 
     def __init__(self, config):
-        self.ocid_key = config.ocid_key
+        # self.ocid_key = config.ocid_key
         # self.ocid_feed_prefix = "http://opencellid.org/downloads/?"
-        self.ocid_feed_prefix = "https://download.unwiredlabs.com/ocid/downloads/?"  # NOQA
-        self.ocid_feed_file = "cell_towers.csv.gz"
-        self.ocid_url = "%stoken=%s&file=%s" % (self.ocid_feed_prefix,
-                                                self.ocid_key,
-                                                self.ocid_feed_file)
+        # self.ocid_feed_prefix = "https://download.unwiredlabs.com/ocid/downloads/?"  # NOQA
+        # self.ocid_feed_file = "cell_towers.csv.gz"
+        self.ocid_url = self.pick_ocid_url_from_list(
+                            self.get_ocid_urls_from_mls_page())
+        # self.ocid_url = "%stoken=%s&file=%s" % (self.ocid_feed_prefix,
+        #                                        self.ocid_key,
+        #                                        self.ocid_feed_file)
         # self.ocid_url = "%sapiKey=%s&filename=%s" % (self.ocid_feed_prefix,
         #                                             self.ocid_key,
         #                                              self.ocid_feed_file)
@@ -34,11 +37,37 @@ class FeedConsumer(object):
         # self.chunk_size = 104857600  # 100MB
         self.chunk_size = None
 
+    @classmethod
+    def get_ocid_urls_from_mls_page(cls):
+        """Extracts OCID urls from MLS downloads page"""
+        mls_downloads_page = "https://location.services.mozilla.com/downloads"
+        dl_page_contents = requests.get(mls_downloads_page).text
+        targets = []
+        rxmatch = r'https://[A-Za-z0-9]+\.cloudfront\.net/export/MLS-full-cell-export-\d{4}-\d{2}-\d{2}T\d+\.csv.gz'  # NOQA
+        for line in dl_page_contents.splitlines():
+            matches = re.findall(rxmatch, line)
+            if matches:
+                targets.extend(matches)
+        return targets
+
+    @classmethod
+    def pick_ocid_url_from_list(cls, url_list):
+        """Gets the link to the most recent MLS download"""
+        date_struct = {}
+        date_list = []
+        for url in url_list:
+            d = re.findall(r'\d{4}-\d{2}-\d{2}T\d+', url)[0]
+            date_struct[d] = url
+            date_list.append(d)
+        target = sorted(date_list)[-1]
+        return date_struct[target]
+
     def write_ocid_feed_file(self):
         """ Calling this method will cause the retrieval of the
         OpenCellID data feed. """
-        payload = {"key": self.ocid_key}
-        response = requests.post(self.ocid_url, data=payload, stream=True)
+        # payload = {"key": self.ocid_key}
+        # response = requests.post(self.ocid_url, data=payload, stream=True)
+        response = requests.get(self.ocid_url, stream=True)
         print "Getting OCID feed file.  This will take a while..."
         with open(self.ocid_outfile, 'wb') as feed_file:
             # status = 0
